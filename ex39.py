f71ae46d3bc6159615a48e8dd92e2e341a123161
csizmaziakiki <kiki.csizmazia@gmail.com>
2019-02-01 21:26:05 +0100

    Here we go again

---
+++ ex39.py
@@ -0,0 +1,13 @@
+stuff = {'name': 'Kiki', 'age': 25}
+print stuff['name']
+print stuff['age']
+
+for key, value in stuff.items():
+	print "%s: %s" % (key, value)
+
+del stuff[name]
+#safely get key that maybe not exists, from value (none is default)
+skey = stuff.get('City', None)
+
+if not skey:
+	print "Sorry, no City" 
